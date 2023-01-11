879779999312ea4403a853b848c8f6fb07f832ec
bakalover <kostya@shprenger.com>
2023-01-12 00:19:20 +0300

    raw bugged awful version

--- program.cpp
+++ program.cpp
@@ -5,6 +5,12 @@ using namespace std;
 struct point{
     double x,y;
 };
+
+double func(struct point point){
+    return point.x*point.x*point.x+point.y*point.y*point.y+point.x;
+}
+
+//------------------------------------VALIDATING-CHAIN----------------------------------------------
 double area (struct point a, struct point b, struct point c) {
 	return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
 }
@@ -18,12 +24,12 @@ bool intersect_parallel (double a, double b, double c, double d) {
 bool intersect (struct point a, struct point b, struct point c, struct point d) {
 	return intersect_parallel (a.x, b.x, c.x, d.x)
 		&& intersect_parallel (a.y, b.y, c.y, d.y)
-		&& area(a,b,c) * area(a,b,d) <= 0
-		&& area(c,d,a) * area(c,d,b) <= 0;
+		&& area(a,b,c) * area(a,b,d) < 0
+		&& area(c,d,a) * area(c,d,b) < 0;
 }
 
-bool check_crossing(vector<struct point> &arr, struct point new_point){
-    for (size_t i = 1; i < arr.size(); i++)
+bool check_crossing_new(vector<struct point> &arr, struct point new_point){
+    for (size_t i = 1; i < arr.size()-1; i++)
     {
         if(intersect(arr[i-1],arr[i],arr[arr.size()-1],new_point)){
             return true;
@@ -32,10 +38,28 @@ bool check_crossing(vector<struct point> &arr, struct point new_point){
     return false;
 }
 
+int32_t check_crossing_internal(vector<struct point> &arr, struct point point_1,struct point point_2){
+    int32_t count = 0;
+    for (size_t i = 1; i <= arr.size()-1; i++)
+    {
+        if(intersect(arr[i-1],arr[i],point_1,point_2)){
+            ++count;
+        }
+    }
+    return count;
+}
+
+//------------------------------------VALIDATING-CHAIN----------------------------------------------
+
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     double x,y,count;
+
     vector<struct point> arr(0);
+    cout<<"Please, draw a polygon by entering point coordinats. Be sure that first and last point - same"<<endl;
     cout<<"Enter number of points: "<<endl;
     while(true){
         cin>>count;
@@ -49,12 +73,16 @@ int main()
         cin.ignore(1000,'\n');
     }
     cout<<"\nEnter points in format: \nx1 y1\nx2 y2\n.\n.\n.\n------------------------"<<endl;
-    for (size_t i = 0; i < count; i++)
+    for (size_t i = 0; i <= count; i++)
     {
         while(true){
             cin>>x>>y;
-            if(check_crossing(arr,(struct point){.x=x,.y=y})&&arr.size()>=2){
+            if(arr.size()>=2&&check_crossing_new(arr,(struct point){.x=x,.y=y})){
                 cout<<"Crossing! Repeat point:"<<endl;
+                continue;
+            }
+            if(i==count&&(arr[0].x!=x||arr[0].y!=y)){
+                cout<<"Polygonal chain is not closed! Repeat point:"<<endl;
                 continue;
             }
             else{
@@ -64,5 +92,26 @@ int main()
         }
     }
     
+    double I = 0;
+    for (int32_t i = -1300*2; i <= -500*2; i++)
+    {
+        for (int32_t j = 2100*2; j <= 9400*2; j++)
+        {
+            struct point check_point = (struct point){.x = (i/200), .y = (j/200)};
+            struct point inf_point = (struct point){.x = 100, .y = 50};
+            if(check_crossing_internal(arr,check_point,inf_point)%2==1){
+                I += func(check_point)*(0.000025);
+                //I += 1*(0.0001);
+            }
+        }
+        
+    }
+    cout<<"Integral value: "<<I<<endl;
+
     return 0;
 }
+// -5 22
+// -6 22
+// -6 23
+// -5 23
+// -5 22
